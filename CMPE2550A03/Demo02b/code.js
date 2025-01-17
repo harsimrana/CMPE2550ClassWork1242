8977c70fd7f57f298c9cca14c881788ca5242b48
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-01-17 14:50:12 -0700

    Final commit 17 Jan 2025

--- CMPE2550A03/Demo02b/code.js
+++ CMPE2550A03/Demo02b/code.js
@@ -2,10 +2,32 @@
 $().ready( ()=> {
     console.log("On page load");
 
+    // On click binding for b1 button
     $("#b1").on("click", ()=>{
         console.log("B1 has been clicked");
+        MakeAjaxCall("server.php", "GET", "HTML", {}, successB1, errorHandler);
+    });
+
+    // On click binding for b2 button
+    $("#b2").on("click", ()=>{
+        console.log("B2 has been clicked");
+        let data = {};
+        data['p1Name'] = "Harsimranjot";
+        data['p2Name'] = "Dustin";
+        data['action'] = "b2"; // To identify request on server side
+
+        MakeAjaxCall("server.php", "GET", "HTML", data, successB2, errorHandler);
     });
 })
+
+// function to handle successful response for B2 button
+function successB2(serverData, serverStatus)
+{
+    console.log("Inside Success handler");
+    console.log(serverData);
+    console.log(serverStatus);
+    $("#serverResponse").html(serverData);
+}
 
 // function to handle successful response for B1 button
 function successB1(serverData, serverStatus)
@@ -13,19 +35,21 @@ function successB1(serverData, serverStatus)
     console.log("Inside Success handler");
     console.log(serverData);
     console.log(serverStatus);
+    $("#serverResponse").html(serverData);
 }
 
 function errorHandler(ajaxReq, ajaxStatus, errorThrown)
 {
     console.log("Inside error handler function");
-    console.log(serverData);
+    console.log(ajaxStatus);
     console.log(errorThrown);
+    $("#serverResponse").html(ajaxStatus);
 }
 
 // MakeAjaxCall is used to make an ajax call
 function MakeAjaxCall(serverURL, reqMethod, serverResponse, data, Successhandler, Errorhander)
 {
-    console.log("inside MakeAjaxCall function")';
+    console.log("inside MakeAjaxCall function");
 
     let ajaxOptions= {};
     ajaxOptions['url']      = serverURL;    // Destination URL
---
+++ CMPE2550A03/Demo02b/server.php
@@ -0,0 +1,33 @@
+<?php
+    error_log("Inside Server.php");
+
+    // Valdiate your request
+    if($_SERVER['REQUEST_METHOD']== "GET")
+    {
+        // Whether request is from B1 or B2 button
+        if( strip_tags( $_GET['action']) == "b2")
+        {
+            // Validate sanitize your data
+
+            session_start();
+            $_SESSION['p1Name'] = strip_tags(trim($_GET['p1Name'])); 
+            $_SESSION['p2Name'] = strip_tags(trim($_GET['p2Name'])); 
+            $_SESSION['gameBoard'] = array();
+
+            // returning response back to client
+            echo "Player  ". $_SESSION['p1Name'] ." will go first ";
+        }
+        else // otherwise it is b1 button
+        {
+        
+            $string = "Welcome to server side";
+            $string .= "Current Turn: 1";
+
+            echo $string;  // Point to return HTML data back to client
+        }
+    }
+    else
+    {
+        echo "No my friend you are not allowed";
+    }
+?>
