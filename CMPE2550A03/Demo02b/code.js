73996da4040737a0b33c093d113bf737e8d97777
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-01-17 14:16:58 -0700

    Second Commjit

--- CMPE2550A03/Demo02b/code.js
+++ CMPE2550A03/Demo02b/code.js
@@ -6,3 +6,37 @@ $().ready( ()=> {
         console.log("B1 has been clicked");
     });
 })
+
+// function to handle successful response for B1 button
+function successB1(serverData, serverStatus)
+{
+    console.log("Inside Success handler");
+    console.log(serverData);
+    console.log(serverStatus);
+}
+
+function errorHandler(ajaxReq, ajaxStatus, errorThrown)
+{
+    console.log("Inside error handler function");
+    console.log(serverData);
+    console.log(errorThrown);
+}
+
+// MakeAjaxCall is used to make an ajax call
+function MakeAjaxCall(serverURL, reqMethod, serverResponse, data, Successhandler, Errorhander)
+{
+    console.log("inside MakeAjaxCall function")';
+
+    let ajaxOptions= {};
+    ajaxOptions['url']      = serverURL;    // Destination URL
+    ajaxOptions['data']     = data;         // Client Data sent to server
+    ajaxOptions['type']     = reqMethod;    // GET/POST
+    ajaxOptions['dataType'] = serverResponse;// HTML/ JSON
+    ajaxOptions['success']  = Successhandler;// Function to call on successful attemp
+    ajaxOptions['error']    = Errorhander;   // Callback function in case of an error
+    
+    //Actually making ajax call now
+
+    $.ajax(ajaxOptions);
+
+}
