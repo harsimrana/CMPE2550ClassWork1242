5531b478b745d2cf86e4c92cff7dd17ac2d6f16f
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-01-30 12:13:18 -0700

    Demo 03 A01 First Commit

---
+++ CMPE2550A01/Demo03DBSelect/dbUtil.php
@@ -0,0 +1,73 @@
+<?php
+    error_log("In DB util file");
+
+    // Database connection information
+    $mysql_connection= null;
+
+
+/****************************************************
+ * mySQLConnection(): Establish connection to DB
+ * Inputs           : No
+ * Output           : No
+ ***************************************************/    
+function mySQLConnection()
+{
+    // Grab hold on to connection variable
+    global $mysql_connection;
+
+    // Try to connect to DB
+    /*
+    mysqli(): Will connect to DB and return connection info
+    1. DB SERVER: localhost
+    2. Username: 
+    3. Password:
+    4. DB Name:
+    */
+                                // DB Server  , Username,          Password,          DB Name
+    $mysql_connection = new mysqli("localhost", "aulakhha_2550A01", "zC7D{Fgq9Z&m", "aulakhha_2550A01");
+
+    if($mysql_connection -> connect_errno)
+    {
+        error_log("Connection error");
+        die();
+    }
+    // if you are here
+    error_log("Connection is successful");
+}
+
+
+/****************************************************
+ * mySelectQuery(): To execute your select queries 
+ * Inputs           : query
+ * Output           : Result set or false
+ ***************************************************/    
+function mySelectQuery($myquery)
+{
+    // Grab hold on to connection variable
+    global $mysql_connection;  
+
+    $results = false;
+
+    // Run the query 
+    // query function will return result set or false
+    if( !($results = $mysql_connection -> query(myquery)))
+    {
+        // error 
+        error_log("Error while executing query ");
+        return false;
+    }
+    else
+    {
+        // All good
+        // Return the result set back to calling location
+        error_log("Inside DBUtil before returning result set");
+        
+        return $results;
+    }
+
+}
+
+// Testing purpose only
+//mySQLConnection();
+
+?>
---
+++ CMPE2550A01/Demo03DBSelect/index.php
@@ -0,0 +1,24 @@
+<?php
+    require_once('dbUtil.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Demo 03</title>
+    </head>
+    <body>
+        <header>
+            <h1> Week 4: Day 03 :Demo 03: Connecting to DB </h1> 
+        </header>
+
+        <main>
+            <?php
+                mySQLConnection();
+            ?>
+        </main>
+
+    </body>
+</html>
