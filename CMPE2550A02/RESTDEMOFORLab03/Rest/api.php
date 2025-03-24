75ed0ed43ac999920eea35e8bc371fe5c5211211
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-03-24 14:52:31 -0600

    Final Commit

---
+++ CMPE2550A02/RESTDEMOFORLab03/Rest/api.php
@@ -0,0 +1,16 @@
+<?php
+error_log("Inside API.php GET:".json_encode($_GET));
+error_log("Inside API.php Request:".json_encode($_REQUEST['request']));
+
+require_once 'apiDef.php';
+
+try
+{
+    $API = new MyAPI($_REQUEST['request']);  // Pass the information to constructor
+    error_log("Inside API.PHP after constructor call");
+    echo $API->processAPI();
+}
+catch(Exception $e)
+{
+    echo json_encode(Array('error' => $e.getMessage()));
+}
