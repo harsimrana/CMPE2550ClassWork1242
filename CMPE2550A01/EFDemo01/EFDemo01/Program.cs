ded356b43b8cf1062e3496c8913e9d7c907d769c
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-04-03 12:40:21 -0600

    Demo EF 01 Done

---
+++ CMPE2550A01/EFDemo01/EFDemo01/Program.cs
@@ -0,0 +1,57 @@
+using EFDemo01.ModelsNWD;
+
+namespace EFDemo01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "EF Demo 01: 03 April 2025");
+
+            // EF - Entitiy FrameWork is object -relation mapper
+            // Step 1 : Install Package - Microsoft.EntityFrameworkCore.SqlServer 
+            // Step 2 : Install a tool Tools-> NuGet Package Manager- Package Manager Console
+            // install-package Microsoft.EntityFrameworkCore.Tools 
+
+            app.MapGet("/RetData", () =>
+            {
+                Console.WriteLine("In Ret Data Endpoint");
+                // Create a object of DB
+                var db = new DemoDb2550NorthwindContext();
+
+                // LINQ queries
+                // Query Style of LINQ queries
+                //PART 1
+                // select * from Products
+                /*
+                   var results = from p in db.Products
+                                    join c in db.Categories
+                                    on p.CategoryId equals c.CategoryId
+                                 where p.ProductId <= 5 // Filtering
+                                 orderby p.ProductId descending // By default it is ascending order Type full descending keyword
+                                  //select p;  // similar to select *
+                                   select new { p,c.CategoryName};  // All columns from p and Just Category Name from c
+                
+                return results.ToList();
+                */
+                // Method Style 
+
+                
+                var results = db.Products
+                                        .Where(x=> x.CategoryId == x.Category.CategoryId)  // JOIN part
+                                        .Where(x=>x.ProductId<= 5)
+                                        //.OrderBy(x=>x.ProductId)  // ascending order
+                                        .OrderByDescending(x=>x.ProductId)  // descending order
+                                        .Select(x => new { x, x.Category.CategoryName});
+
+                return results.ToList();
+                
+            });
+
+            app.Run();
+        }
+    }
+}
