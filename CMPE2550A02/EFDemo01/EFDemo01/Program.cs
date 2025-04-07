0a8785bd55e7c1b8511304cb9376486138acb9b4
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-04-07 14:39:28 -0600

    EF Demo 02 Done 07.04.2025

--- CMPE2550A02/EFDemo01/EFDemo01/Program.cs
+++ CMPE2550A02/EFDemo01/EFDemo01/Program.cs
@@ -30,37 +30,154 @@ namespace EFDemo01
                     // UNCOMMENT THE FOLLOWING TO TEST IT WITH QUERY STYLE
                     //select * from products
 
-                   /*
-                        var results = from p in db.Products
-                                      join c in db.Categories
-                                        on p.CategoryId equals c.CategoryId
-                                      where p.ProductId <= 10  // filtering results
-                                      orderby p.ProductId descending  // sorting - by default it is ascending use keyword descending
-                                      //select p;  // similar to select * 
-                                      select new { p, c.CategoryName};  // Target specific columns 
+                    /*
+                         var results = from p in db.Products
+                                       join c in db.Categories
+                                         on p.CategoryId equals c.CategoryId
+                                       where p.ProductId <= 10  // filtering results
+                                       orderby p.ProductId descending  // sorting - by default it is ascending use keyword descending
+                                       //select p;  // similar to select * 
+                                       select new { p, c.CategoryName};  // Target specific columns 
 
-                        return results.ToList();
-                   */
+                         return results.ToList();
+                    */
 
                     //Part 2
                     // Method Style
                     // UNCOMMENT THE FOLLOWING TO TEST IT WITH Method STYLE of LINQ queries
-                
+
                     var results = //db.Products.Select(x => x);  // Select * from Products
                                     db.Products
-                                     .Where(x=> x.CategoryId == x.Category.CategoryId)
-                                     .Where(x=> x.ProductId <= 10)  // Filtering the results
-                                     //.OrderBy(x=>x.ProductId)  // SORTING - ascending order
-                                     .OrderByDescending(x=>x.ProductId) // decending order - specific function for each one
-                                     .Select(x => new { x.ProductId, x.ProductName, x.Category.CategoryName});
+                                     .Where(x => x.CategoryId == x.Category.CategoryId)
+                                     .Where(x => x.ProductId <= 10)  // Filtering the results
+                                                                     //.OrderBy(x=>x.ProductId)  // SORTING - ascending order
+                                     .OrderByDescending(x => x.ProductId) // decending order - specific function for each one
+                                     .Select(x => new { x.ProductId, x.ProductName, x.Category.CategoryName });
                     return results.ToList();
-               
+
 
                 }
 
 
 
             });
+
+            // DML PART -INSERT, UPDATE AND DELETE
+            // For testing purpose using MapGet only- make sure to use REST methods
+            app.MapGet("InsertCategory", () =>
+            {
+                Console.WriteLine("Inside Insert Category");
+
+                var db = new DemoDb2550NorthwindContext();
+
+                // Create an object of category class
+
+                Category newCategory = new Category();
+
+                // Populate the values for the required properties
+
+                newCategory.CategoryName = "NewTest";
+                newCategory.Description = "New Desc";
+
+                try
+                {
+                    // using add method you can add a new object into the list
+                    db.Categories.Add(newCategory);
+
+                    // Save the changes in the DB
+                    db.SaveChanges(); // == Commit
+                                      // 
+                    Console.WriteLine("Inserted successfully");
+                    return "Inserted Successfully";
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error " + e.Message);
+
+                    return "Error " + e.Message;
+                }
+            });
+
+            app.MapGet("DeleteCategory/{cid}", (string cid) =>
+            {
+                Console.WriteLine($" Inside delete Category {cid}");
+                try
+                {
+                    int categoryId = int.Parse(cid);
+
+                    var db = new DemoDb2550NorthwindContext();
+
+                    //Try to delete category form DB
+                    if (db.Categories.Find(categoryId) is Category c)
+                    { // if match found, it will return an object
+                        // Remove that oject from LIST
+
+                        db.Categories.Remove(c);
+                        db.SaveChanges();
+
+                        return "Deleted successfully";
+                    }
+                    else
+                    {
+                        return $"Category with id {categoryId} is not found";
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error " + e.Message);
+
+                    return "Error " + e.Message;
+                }
+
+            });
+
+            app.MapGet("UpdateCategory/{cid}/{cname}", (string cid, string cname) =>
+            {
+                Console.WriteLine($" Inside Update Category {cid}");
+                try
+                {
+                    int categoryId = int.Parse(cid);
+
+                    var db = new DemoDb2550NorthwindContext();
+
+                    //Try to Update category form DB
+                    if (db.Categories.Find(categoryId) is Category c)
+                    { // if match found, it will return an object
+                        // Update that oject from LIST
+
+                        // Now you have object you can make changes
+
+                        c.CategoryName = cname; // New name
+                        c.Description = "Testing with hard code value here";
+
+
+                        db.Categories.Update(c);  // updating object in the list
+
+                    
+                        db.SaveChanges(); // saving changes 
+
+                        return "Update successfully";
+                    }
+                    else
+                    {
+                        return $"Category with id {categoryId} is not found";
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error " + e.Message);
+
+                    return "Error " + e.Message;
+                }
+
+            });
+
 
             app.Run();
         }
--- CMPE2550A02/EFDemo01/EFDemo01/obj/Debug/net8.0/EFDemo01.AssemblyInfo.cs
+++ CMPE2550A02/EFDemo01/EFDemo01/obj/Debug/net8.0/EFDemo01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFDemo01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02060e459a01f1d1c603bf6e3a4bd3e1d94a13b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5a9812167726c6a5ec01b447f66c5ca52f1e7c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFDemo01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFDemo01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
